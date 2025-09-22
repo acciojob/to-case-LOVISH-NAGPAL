aba2a2008da98dadc17e4876119c1f29210d695a
LOVISH-NAGPAL <lovishnagpal0001@gmail.com>
2025-09-22 08:00:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ return `${text.toLowerCase()}-${text.toUpperCase()}`
 	
 }
 
-DO not change the code below
+// DO not change the code below
 
 const text = prompt("Enter text:");
 alert(toCase(text));
